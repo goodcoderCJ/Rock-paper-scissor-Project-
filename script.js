9275f882110a220e8f3eb8d45a4fdc7738988761
Etumnu Chinenye-goodcoderCj <chinenyejuliet2019@gmail.com>
2023-02-01 14:54:54 +0100

    Added animation circle and completed game replay functionalities

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ const winLoseTextElem = document.querySelector(".win");
 const replay = document.querySelector(".replay");
 let scoreElement = document.querySelector(".score");
 let playerComPickedText = document.querySelector(".playerComPicked");
+const beforeMainComp = document.querySelector(".computer-container");
 
 //variables for processing game
 let iconState = false;
@@ -36,6 +37,9 @@ let compSelection;
 let WinText;
 let score = 0;
 
+let playerCount = 0;
+let computerCount = 0;
+
 // getting computer selection
 function compSelectedIcon() {
   let iconsLength = getEachIcon.length;
@@ -44,26 +48,31 @@ function compSelectedIcon() {
   return compSelection;
 }
 
-function game() {
-  getEachIcon.forEach((icon) => {
-    icon.addEventListener("click", (e) => {
-      //getting player selection
-      playerSelection = e.currentTarget.dataset.player;
+function getResult(e) {
+  //getting player selection
+  playerSelection = e.currentTarget.dataset.player;
+  iconState = true;
 
-      console.log(playerSelection);
+  console.log(playerSelection);
 
-      compSelection = compSelectedIcon();
+  compSelection = compSelectedIcon();
 
-      console.log(compSelection);
-      displayItems();
-      determineWinner();
-    });
-  });
+  console.log(compSelection);
+  displayItems();
 }
+//variables for showing selected item
 let playerImage = document.createElement("img");
 let playerImageWrap = document.createElement("div");
 let compImage = document.createElement("img");
 let compImageWrap = document.createElement("div");
+
+// displayGameOver message element
+
+let newGameOverElement = document.createElement("div");
+newGameOverElement.classList.add("newElementWrap");
+let newElement = document.createElement("div");
+newElement.classList.add("new-element");
+//show selected item function
 function displayItems() {
   playerImageWrap.classList.add("image-wrap");
   playerImage.classList.add(`icon-${playerSelection}`);
@@ -79,15 +88,33 @@ function displayItems() {
   compImageWrap.appendChild(compImage);
 
   iconsWrap.style.display = "none";
+  beforeMainComp.style.display = "flex";
+  compSelectionWrap.style.display = "none";
   iconShowAfterSelect.style.display = "flex";
   playerComPickedText.style.display = "flex";
+  let gameFeedback = document.querySelector(".game-feedback");
+  gameFeedback.style.display = "none";
+  setTimeout(() => {
+    compSelectionWrap.style.display = "flex";
+    beforeMainComp.style.display = "none";
+    gameFeedback.style.display = "block";
+    let exactWinner = determineWinner;
+    exactWinner();
+  }, 3000);
 }
+
+//variables for circle animation
+const firstCircle = document.querySelector(".first-circle");
+const secondCircle = document.querySelector(".second-circle");
+const thirdCircle = document.querySelector(".third-circle");
+const animateContainer = document.querySelector(".animate");
+// function to determine winner
 function determineWinner() {
   if (playerSelection === "paper") {
     if (compSelection === "rock") {
       WinText = "YOU WIN";
     } else if (compSelection === "scissors") {
-      WinText = " YOU LOSE";
+      WinText = "YOU LOSE";
     } else {
       WinText = "DRAW GAME";
     }
@@ -111,17 +138,54 @@ function determineWinner() {
   switch (WinText.toLowerCase()) {
     case "you win":
       score++;
+      winningSound.play();
+      animateContainer.style.display = "block";
+      interval = setInterval(() => {
+        firstCircle.style.transform = `scale(1)`;
+        secondCircle.style.transform = `scale(1.2)`;
+        thirdCircle.style.transform = `scale(1.5)`;
+        setInterval(() => {
+          firstCircle.style.transform = `scale(1.2)`;
+          secondCircle.style.transform = `scale(1.5)`;
+          thirdCircle.style.transform = `scale(1.7)`;
+        }, 1000);
+      }, 2000);
       break;
     case "you lose":
       score--;
+      animateContainer.style.display = "none";
+      clearInterval(interval);
       break;
     case "draw game":
       score = score;
+      animateContainer.style.display = "block";
   }
+
   scoreElement.textContent = score;
   winLoseTextElem.textContent = `${WinText}`;
+  WinText.toLowerCase() === "you win"
+    ? playerCount++
+    : WinText.toLowerCase() === "you lose"
+    ? computerCount++
+    : (playerCount = playerCount),
+    (computerCount = computerCount);
+  console.log(playerCount);
+  console.log(computerCount);
+  if (playerCount === 5 || computerCount === 5) {
+    let resultEnclose = document.querySelector(".result-enclose");
+
+    newElement.textContent = `Game Over, You scored ${score} point${
+      score !== 1 ? "s" : ""
+    }`;
+    newElement.appendChild(newGameOverElement);
+    resultEnclose.insertAdjacentElement("afterend", newElement);
+    newElement.style.display = "flex";
+    let gameFeedback = document.querySelector(".game-feedback");
+    gameFeedback.style.bottom = "-0.3em";
+  }
 }
 function replayGame() {
+  winningSound.pause();
   iconsWrap.style.display = "block";
   iconShowAfterSelect.style.display = "none";
   playerComPickedText.style.display = "none";
@@ -129,7 +193,19 @@ function replayGame() {
   compSelectionWrap.classList.remove(compSelection);
   compSelection = "";
   playerSelection = "";
+  if (playerCount === 5 || computerCount === 5) {
+    newElement.style.display = "none";
+    score = 0;
+    playerCount = 0;
+    computerCount = 0;
+  }
 }
-replay.addEventListener("click", replayGame);
+let winningSound = new Audio(
+  "./audio/mixkit-huge-crowd-cheering-victory-462.wav"
+);
 
-game();
+getEachIcon.forEach((icon) => {
+  icon.addEventListener("click", getResult);
+});
+
+replay.addEventListener("click", replayGame);
